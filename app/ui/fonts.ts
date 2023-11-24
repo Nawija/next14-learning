3a9a955622791ce4ef30f8a85174ef260553332b
Valkon <96625039+Nawija@users.noreply.github.com>
2023-11-24 19:59:23 +0100

    v1

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana, Raleway } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ weight: ['400','700'], subsets: ['latin'] });
+export const raleway = Raleway({ weight: ['400','500'], subsets: ['latin'] });
