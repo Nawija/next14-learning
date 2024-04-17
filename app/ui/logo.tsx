ab3c1ffeeed017a846a1b031e77bc1cb60554591
Valkon <96625039+Nawija@users.noreply.github.com>
2024-04-17 09:20:16 +0200

    fg

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -7,7 +7,7 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <AiOutlineGitlab className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[40px]">Perlon</p>
+      <p className="text-[40px]">Logo</p>
     </div>
   );
 }
